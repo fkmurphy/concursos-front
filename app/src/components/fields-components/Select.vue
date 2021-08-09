8d9028723c1c7dd224b65cd9dfd000508b62454f
Julian Murphy <njmdistrisoft@gmail.com>
2021-08-09 01:07:07 -0300

    add component fields

---
+++ app/src/components/fields-components/Select.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <label class="block text-sm font-bold text-gray-700" :for="name">
+     {{label.charAt(0).toUpperCase() + label.slice(1)}}
+    </label>
+    <select
+        :value="value"
+        class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow focus:outline-none focus:shadow-outline"
+        :id="name"
+        type="text"
+        :placeholder="placeholder ?? label">
+        <option v-for="(option, key) in data" :key="key" :value="key" :selected="value === key">{{option}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Text',
+  props: {
+    label: String,
+    name: String,
+    value: String,
+    data: Object,
+    placeholder: String
+  },
+  components: {
+  },
+  setup() {
+  },
+  computed: {
+    defineComponent()
+        {
+            if (this.type === "action") {
+                return "Actions"
+            }
+            if (this.type === "status") {
+                return "Status"
+            }
+            return "ColumnString"
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ app/src/components/fields-components/Text.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <label class="block text-sm font-bold text-gray-700" :for="name">
+     {{label.charAt(0).toUpperCase() + label.slice(1)}}
+    </label>
+    <input
+        class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+        :id="name"
+        type="text"
+        :value="value"
+        :placeholder="placeholder ?? label"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Text',
+  props: {
+    label: String,
+    name: String,
+    value: String,
+    placeholder: String
+  },
+  components: {
+  },
+  setup() {
+  },
+  computed: {
+    defineComponent()
+        {
+            if (this.type === "action") {
+                return "Actions"
+            }
+            if (this.type === "status") {
+                return "Status"
+            }
+            return "ColumnString"
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
