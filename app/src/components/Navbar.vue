081d8ee95741b33d30c23e37d83b9247148f09e7
Julian Murphy <njmdistrisoft@gmail.com>
2021-11-06 18:44:12 -0300

    navbar profile

--- app/src/components/Navbar.vue
+++ app/src/components/Navbar.vue
@@ -55,27 +55,13 @@
         <!-- Profile dropdown -->
         <div class="ml-3 relative">
           <div>
-            <button @click="profileOpen = !profileOpen" type="button" class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-              <span class="sr-only">Open user menu</span>
-              <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-            </button>
+              <button class="text-gray-400" @click="showProfileClick()" href="#">Nombre</button>
           </div>
 
-          <!--
-            Dropdown menu, show/hide based on menu state.
-
-            Entering: "transition ease-out duration-100"
-              From: "transform opacity-0 scale-95"
-              To: "transform opacity-100 scale-100"
-            Leaving: "transition ease-in duration-75"
-              From: "transform opacity-100 scale-100"
-              To: "transform opacity-0 scale-95"
-          -->
-          <div v-show="profileOpen" class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+          <div v-show="showProfile == true" class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
             <!-- Active: "bg-gray-100", Not Active: "" -->
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Editar perfil</a>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Salir</a>
           </div>
         </div>
       </div>
@@ -95,14 +81,20 @@
 </template>
 
 <script>
+import {ref} from 'vue';
 export default {
-  name: 'Navbar',
-  props: {
-    msg: String
-  },
-  setup() {
+    name: 'Navbar',
+    props: {
+        msg: String
+    },
+    setup() {
+        let showProfile = ref(false);
+        let showProfileClick = () => {
+            showProfile.value = !showProfile.value;
+        }
 
-  },
+        return { showProfile, showProfileClick };
+    }
 }
 </script>
 
